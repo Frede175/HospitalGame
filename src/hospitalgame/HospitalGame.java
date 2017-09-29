7603b7ea91d30db667506dbfc9b148ea593f561a
toszi <toszi18@gmail.com>
2017-09-29 10:41:19 +0200

    Made the game play

--- src/hospitalgame/HospitalGame.java
+++ src/hospitalgame/HospitalGame.java
@@ -16,6 +16,9 @@ public class HospitalGame {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Game game = new Game();
+        game.play();
     }
     
 }
