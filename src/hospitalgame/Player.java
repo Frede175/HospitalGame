30dc1adffbcfa32b10c0d6eb32a10f2975bfaac6
Frede175 <fsr1998@hotmail.com>
2017-10-12 14:31:25 +0200

    Added authors

--- src/hospitalgame/Player.java
+++ src/hospitalgame/Player.java
@@ -1,13 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hospitalgame;
 
 /**
  *
- * @author fsr19
+ * @author Frederik Schultz Rosenberg
+ * @author Andreas Bøgh Mølgaard-Andersen
+ * @author Tobias Ahrenschneider Sztuk
+ * @author Lars Bjerregaard Jørgensen
+ * @author Robert Francisti
+ * @author Michael Kolling and David J. Barnes
  */
 public class Player {
     
