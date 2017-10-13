97741e26fc6b5c94208aaf18fb789d1066b3cedc
Frede175 <fsr1998@hotmail.com>
2017-10-13 15:36:03 +0200

    Format

--- src/hospitalgame/Player.java
+++ src/hospitalgame/Player.java
@@ -15,50 +15,50 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  * @author Robert Francisti
  */
 public class Player {
-    
+
     /**
      * The players blood type
      */
     private BloodType bloodType;
-    
+
     /**
      * The room the player is currently in
      */
     private Room currentRoom;
-    
+
     /**
      * Players inventory that holds the players items
      */
     private Inventory inventory;
-    
+
     /**
      * The player name
      */
     private String name;
-    
+
     /**
      * The amount of blood the player has left
      */
     private double bloodAmount;
-    
+
     /**
      * The default amount of blood the player loses
      */
     private double bloodRate;
-    
+
     /**
      * When the last update to bloodAmount happened
      */
     private long lastUpdate;
-    
+
     /**
      * The items that are active. These items apply effects on the player.
      */
     private ArrayList<Item> activeItems;
-    
-    
+
     /**
      * Constructor for player
+     *
      * @param bloodType the player have.
      * @param bloodRate the default amount of blood the player losses.
      * @param bloodAmount the amount of blood the player.
@@ -71,81 +71,88 @@ public class Player {
         this.bloodAmount = bloodAmount;
         this.name = name;
         this.currentRoom = startRoom;
-        
+
         inventory = new Inventory(2000);
-        
+
         lastUpdate = 0;
-        
+
         activeItems = new ArrayList<>();
     }
-    
+
     /**
      * Uses the item that is specified in the command.
+     *
      * @param command the use item command
      */
     public void useItem(Command command) {
         throw new NotImplementedException();
     }
-    
+
     /**
      * Added the item to the inventory.
+     *
      * @param item The item the be added
      * @return true if the item is successfully added to the inventory
      */
     public boolean addItem(Item item) {
         return inventory.addItem(item);
     }
-    
+
     /**
-     * Removes the current item 
+     * Removes the current item
+     *
      * @param item the item to be removed
      * @return true if the item is removed successfully from the inventory.
      */
     public boolean removeItem(Item item) {
         return inventory.removeItem(item);
     }
-    
+
     /**
      * Get the current room that the player is in.
+     *
      * @return the current room.
      */
     public Room getCurrentRoom() {
         return currentRoom;
     }
-    
+
     /**
      * The blood amount the player has.
+     *
      * @return the blood amount.
      */
     public double getBloodAmount() {
         return bloodAmount;
     }
-    
+
     /**
      * Print the contents of the inventory to the console.
      */
     public void showInventory() {
         inventory.showItems();
     }
-    
+
     /**
-     * Gets the item the specified name. 
-     * @param name The name of items 
+     * Gets the item the specified name.
+     *
+     * @param name The name of items
      * @return an array of items that has the specified name.
      */
     public Item[] getItemsByName(ItemName name) {
         return inventory.getItemsByName(name);
     }
-    
+
     /**
-     * Goes the to next room with the given exit. 
+     * Goes the to next room with the given exit.
+     *
      * @param command the go command
      * @return true if the player was able to go the requested room
      */
     public boolean goRoom(Command command) {
         update(); //Updating blood loss
-        
-        if(!command.hasSecondWord()) {
+
+        if (!command.hasSecondWord()) {
             System.out.println("Go where?");
             return false;
         }
@@ -157,32 +164,32 @@ public class Player {
         if (nextRoom == null) {
             System.out.println("There is no door!");
             return false;
-        }
-        else {
+        } else {
             currentRoom = nextRoom;
             System.out.println(currentRoom.getLongDescription());
             return true;
         }
     }
-    
+
     /**
      * Process the take item command.
+     *
      * @param command the take item command
      */
     public void takeItem(Command command) {
         if (command.hasSecondWord()) {
             int index;
-            
+
             //Getting the index(int) from the command
-            try { 
+            try {
                 index = Integer.parseInt(command.getSecondWord());
             } catch (NumberFormatException ex) {
                 System.out.println("That is not a number!");
                 return;
             }
-            
+
             Item item = currentRoom.getItem(index);
-            
+
             if (item != null) { //Checking if the given index has an item
                 if (addItem(item)) { //If addItem returns false, then the item has not been added to the inventory
                     currentRoom.removeItem(item);
@@ -196,9 +203,9 @@ public class Player {
             System.out.println("Take what?");
         }
     }
-    
+
     /**
-     * Calculates the current blood loss and updates the players blood amount. 
+     * Calculates the current blood loss and updates the players blood amount.
      */
     private void update() {
         throw new NotImplementedException();
