5f5bcb5841ed3faf43d103737156545d61ab5aa7
toszi <toszi18@gmail.com>
2017-10-13 12:43:55 +0200

    removed "," from param in javadoc

--- src/hospitalgame/Room.java
+++ src/hospitalgame/Room.java
@@ -92,7 +92,7 @@ public class Room
     
     /**
      * Make it possible to add a specific item to a room
-     * @param item, is an Item object that tells which item should be added
+     * @param item is an Item object that tells which item should be added
      * @return true if it is possible to drop an item from the inventory
      */
     public boolean addItem(Item item){
@@ -101,7 +101,7 @@ public class Room
     
     /**
      * Makes it possible to remove a specific item in the current room
-     * @param item, is which item that should be removed from the room 
+     * @param item is which item that should be removed from the room 
      * @return true if it is possible for the player to pick up the item, adding it to the inventory and removes it from the room
      */
     public boolean removeItem(Item item){
@@ -110,7 +110,7 @@ public class Room
     
     /**
      * Finds a item in the ArrayList, if nothing is found it will return null
-     * @param index, is which index of the ArrayList that the item should be found
+     * @param index is which index of the ArrayList that the item should be found
      * @return an specific Item object 
      */    
     public Item getItem(int index){
