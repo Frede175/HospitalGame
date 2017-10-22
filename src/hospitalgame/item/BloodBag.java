2b490fae62460c82dae6a2feab794d4df0e48e6c
AndreasBoegh <32385164+AndreasBoegh@users.noreply.github.com>
2017-10-22 11:49:10 +0200

    BloodBag implemented

--- src/hospitalgame/item/BloodBag.java
+++ src/hospitalgame/item/BloodBag.java
@@ -1,6 +1,8 @@
 
 package hospitalgame.item;
 
+import hospitalgame.BloodType;
+
 /**
  * @author Frederik Schultz Rosenberg
  * @author Andreas Bøgh Mølgaard-Andersen
@@ -10,18 +12,19 @@ package hospitalgame.item;
  */
 
 public class BloodBag extends BonusPointItem {
-    //private BloodType bloodType;
+
+    private final BloodType bloodType;
 
     /**
      * constructs and initializes the BloodBag object
+     * @param bloodType is the bloodtype of the bloodbag
      * @param bonusPoints the bonusPoints the item is to hold
      * @param name is the name of the item
      * @param weight is the weight of the item
      */
-    public BloodBag(/*BloodType bloodType, */int bonusPoints, ItemName name, int weight) {
+    public BloodBag(BloodType bloodType, int bonusPoints, ItemName name, int weight) {
         super(bonusPoints, name, weight);
-        
-        //this.bloodType = bloodType;
+        this.bloodType = bloodType;
         
     }
     
