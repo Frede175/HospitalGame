153791e70e51034d13443fea0f82c588bbd03734
Jazzmano <robertfrancisti@hotmail.com>
2017-10-23 09:35:16 +0200

    Computer implemented

--- src/hospitalgame/NPC/Computer.java
+++ src/hospitalgame/NPC/Computer.java
@@ -1,4 +1,5 @@
 package hospitalgame.NPC;
+
 import hospitalgame.*;
 
 /**
@@ -8,24 +9,28 @@ import hospitalgame.*;
  * @author Lars Bjerregaard Jørgensen
  * @author Robert Francisti
  */
-public class Computer extends NPC{
-    
-     /**
+public class Computer extends NPC {
+
+    /**
      * Calls the NPC constructor through the super
+     *
      * @param currentRoom the room that the NPC is in right now
-     * @param name name of the NPC  
-     * @param description decription of the NPC 
+     * @param name name of the NPC
+     * @param description decription of the NPC
      */
-    public Computer (Room currentRoom, String name, String description){
+    public Computer(Room currentRoom, String name, String description) {
         super(currentRoom, name, description);
+
     }
 
     /**
      * Overrides the abstract method in NPC
+     *
      * @param player is the player object
      */
     @Override
     public void interact(Player player) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("You have the blood type : " + player.getBloodType());
     }
+
 }
