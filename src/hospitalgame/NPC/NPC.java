c21e6c0e2f5334536f77da4013c19fdecc0b8123
Jazzmano <robertfrancisti@hotmail.com>
2017-10-23 09:37:53 +0200

    NPC implemented

--- src/hospitalgame/NPC/NPC.java
+++ src/hospitalgame/NPC/NPC.java
@@ -1,6 +1,6 @@
 package hospitalgame.NPC;
+
 import hospitalgame.*;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import java.util.Scanner;
 
 /**
@@ -11,67 +11,64 @@ import java.util.Scanner;
  * @author Robert Francisti
  */
 public abstract class NPC {
+
     /**
      * Name of the NPC
      */
-    
     private String name;
     Scanner scanner = new Scanner(System.in);
-    String porterName = "Porter";
-    String doctorName = "Doctor";
-    String ComputerName = "Computer";
-    
-    
+
     /**
      * Description of the NPC
      */
     private String description;
-    
+
     /**
      * Room object to tell which the NPC is in
      */
     private Room currentRoom;
-    
+
     /**
      * Constructor of the NPC objects
+     *
      * @param currentRoom is which room the NPC is in
      * @param name is the name of the NPC
      * @param description gives a decription of the NPC
      */
-    public NPC(Room currentRoom, String name, String description){
+    public NPC(Room currentRoom, String name, String description) {
+        this.currentRoom = currentRoom;
+        this.description = description;
+        this.name = name;
 
     }
-    
+
     /**
-     * 
+     *
      * @return which room the NPC is in
      */
-    public Room getCurrentRoom(){
-        return Room;
-//throw new NotImplementedException();
+    public Room getCurrentRoom() {
+        return currentRoom;
     }
-    
+
     /**
-     * 
+     *
      * @return the name of the NPC
      */
-    public String getName(){
-        //throw new NotImplementedException();
+    public String getName() {
         return name;
-        
     }
-    
+
     /**
-     * 
+     *
      * @return the description of which NPC the player is interacting with
      */
-    public String getDescription(){
-        //throw new NotImplementedException();
+    public String getDescription() {
         return description;
     }
-    
+
     /**
      * has no function, but will call a method of the same name at a object
+     *
      * @param player is a reference to the player
      */
     public abstract void interact(Player player);
