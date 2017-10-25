6dabd2d2fdb549e879150d25fc18482df8d6498b
Jazzmano <robertfrancisti@hotmail.com>
2017-10-25 13:07:28 +0200

    This should fix HospitalGame.java

--- src/hospitalgame/HospitalGame.java
+++ src/hospitalgame/HospitalGame.java
@@ -1,7 +1,5 @@
 package hospitalgame;
 
-import java.util.Scanner;
-
 /**
  * HospitalGame contains the main method
  * 
@@ -18,17 +16,12 @@ public class HospitalGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your score : ");
-       
-            
-        }
         
         /**
          * Make the game run
          */
         Game game = new Game();
-        
+        game.play();
     }
     
-
+}
