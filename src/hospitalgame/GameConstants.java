c2d4a8900cbad810ef2449be8a2323470f4bad17
Lars Jørgensen <lars.b.jn@gmail.com>
2017-10-25 12:45:02 +0200

    Added GameConstants
    * Added GameConstants

---
+++ src/hospitalgame/GameConstants.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospitalgame;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ *
+ * @author larsjorgensen
+ */
+public interface GameConstants {
+    double BANDAGE_BUFF = 1.0;
+    int BANDAGE_WEIGHT = 100;
+    long BANDAGE_TIME = 15;
+    double MORPHINE_BUFF = 1.0;
+    long MORPHINE_TIME = 15;
+    int MORPHINE_WEIGHT = 50;
+    int BLOODBAG_SIZE = 450;
+    ArrayList<String> directions = new ArrayList<>(Arrays.asList("north", "east", "south", "west"));
+}
