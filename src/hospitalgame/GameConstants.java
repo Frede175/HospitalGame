1842f2acc8f9bcf3fb390f4863c7ef152ba762fd
Lars Jørgensen <lars.b.jn@gmail.com>
2017-10-26 15:21:10 +0200

    Fixed buff time
    * Fixed buff times

--- src/hospitalgame/GameConstants.java
+++ src/hospitalgame/GameConstants.java
@@ -13,11 +13,12 @@ import java.util.Arrays;
  * @author Robert Francisti
  */
 public interface GameConstants {
+    // Times is in milliseconds
     double BANDAGE_BUFF = 1.0;
     int BANDAGE_WEIGHT = 100;
-    long BANDAGE_TIME = 15;
+    long BANDAGE_TIME = 15000;
     double MORPHINE_BUFF = 1.0;
-    long MORPHINE_TIME = 15;
+    long MORPHINE_TIME = 15000;
     int MORPHINE_WEIGHT = 50;
     int BLOODBAG_SIZE = 450;
     double PLAYER_BLOODRATE = 10.0;
