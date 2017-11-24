7d462c3e342970431743432297150b50bdd1de90
Jazzmano <robertfrancisti@hotmail.com>
2017-11-24 14:12:32 +0100

    extends DataItem

--- HospitalGame_Business/src/business/Data/DataBonusPointItem.java
+++ HospitalGame_Business/src/business/Data/DataBonusPointItem.java
@@ -5,6 +5,7 @@
  */
 package business.Data;
 
+import business.Item.Item;
 import common.IBonusPointItem;
 import common.IItem;
 import common.ItemName;
@@ -13,23 +14,13 @@ import common.ItemName;
  *
  * @author andreasmolgaard-andersen
  */
-public class DataBonusPointItem implements IBonusPointItem, IItem{
+public class DataBonusPointItem extends DataItem implements IBonusPointItem {
 
     public int bonusPoints;
-    
+
     @Override
     public int getBonusPoints() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public int getWeight() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public ItemName getName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
