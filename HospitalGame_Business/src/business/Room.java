ccaa1b9760eaec07240c0fa26aa767e24e998e7e
AndreasBoegh <32385164+AndreasBoegh@users.noreply.github.com>
2017-11-24 13:08:52 +0100

    Room datafield and method headers added

--- HospitalGame_Business/src/business/Room.java
+++ HospitalGame_Business/src/business/Room.java
@@ -5,10 +5,74 @@
  */
 package business;
 
+import business.common.IItemFacade;
+import common.Directions;
+import common.IItem;
+import common.IRoom;
+import java.util.HashMap;
+import java.util.Set;
+
 /**
  *
  * @author andreasmolgaard-andersen
  */
-public class Room {
+public class Room implements IRoom {
+    private IItemFacade itemFacade;
+    private String name;
+    private HashMap<String, IRoom> exits;
+    private boolean inspected;
+    
+    public Room(String name){
+        
+    }
+    
+    public void injectItemFacade(IItemFacade itemFacade){
+        
+    }
+    
+    public void setExit(Directions direction, IRoom roomNeighbour) {
+        
+    }
+   
+    public boolean addItem(IItem item) {
+        return false;
+    }
+    
+    public boolean removeItem(IItem item) {
+        return false;
+    }
+    
+    public IItem getItem(int index) {
+        return null;
+    }
+    
+    public Set getKeySet() {
+        return null;
+    }
+    
+    @Override
+    public IRoom getExit(Directions direction){
+        return null;   
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Set<String> getExitStrings() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isLocked() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isInspected() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
