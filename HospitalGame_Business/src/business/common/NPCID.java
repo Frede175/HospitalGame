e2a85fb18fa81886131bc532971bfa6a540ea2f3
AndreasBoegh <32385164+AndreasBoegh@users.noreply.github.com>
2017-11-24 13:27:56 +0100

    NPCID enum added

--- HospitalGame_Business/src/business/common/NPCID.java
+++ HospitalGame_Business/src/business/common/NPCID.java
@@ -10,5 +10,21 @@ package business.common;
  * @author andreasmolgaard-andersen
  */
 public enum NPCID {
+    DOCTOR("Doctor"), 
+    COMPUTER("Computer"), 
+    PORTER("Porter");
+    
+    private String name;
+    
+    private NPCID(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
     
 }
+
+
