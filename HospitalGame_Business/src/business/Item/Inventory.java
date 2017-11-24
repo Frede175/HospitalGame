dd1835e7900d39f8cf1f5cf1c1b6495540543a1f
AndreasBoegh <32385164+AndreasBoegh@users.noreply.github.com>
2017-11-24 14:20:14 +0100

    Inventory datafield and method headers added

--- HospitalGame_Business/src/business/Item/Inventory.java
+++ HospitalGame_Business/src/business/Item/Inventory.java
@@ -5,10 +5,51 @@
  */
 package business.Item;
 
+import common.IInventory;
+import common.IItem;
+import common.ItemName;
+import java.util.ArrayList;
+
 /**
  *
  * @author andreasmolgaard-andersen
  */
 public class Inventory {
-    
+
+    private int maxWeight;
+    private int id;
+    private ArrayList<Item> items = new ArrayList<>();
+    private int nextID;
+
+    public Inventory(int maxWeight) {
+
+    }
+
+    public Inventory(IInventory inventory) {
+
+    }
+
+    public boolean addItem(IItem item) {
+        return false;
+    }
+
+    public boolean removeItem(IItem item) {
+        return false;
+    }
+
+    public IItem getItem(int index) {
+        return null;
+    }
+
+    public ArrayList<IItem> getItems() {
+        return null;
+    }
+
+    public IItem[] getItemByName(ItemName name) {
+        return null;
+    }
+
+    public int getTotalWeight() {
+        return 0;
+    }
 }
