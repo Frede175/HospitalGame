a2c99fe319b533832752a427c87ed8baebc725ae
Jazzmano <robertfrancisti@hotmail.com>
2017-11-24 13:52:18 +0100

    variable and method headder

--- HospitalGame_Business/src/business/Data/DataPowerUpItem.java
+++ HospitalGame_Business/src/business/Data/DataPowerUpItem.java
@@ -5,10 +5,25 @@
  */
 package business.Data;
 
+import common.IPowerUpItem;
+
 /**
  *
  * @author andreasmolgaard-andersen
  */
-public class DataPowerUpItem {
+public class DataPowerUpItem implements IPowerUpItem {
+    double buff;
+    long timeLeftOfBuff;
+    long lastUpdate;
+
+    @Override
+    public double getBuff() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long getTimeLeftOfBuff() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
