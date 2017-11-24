dd706bb401b0364f8054e8116790c7ef9b98f5ea
AndreasBoegh <32385164+AndreasBoegh@users.noreply.github.com>
2017-11-24 13:34:19 +0100

    INPCFacade methods and datafield added

--- HospitalGame_Business/src/business/NPC/NPCFacade.java
+++ HospitalGame_Business/src/business/NPC/NPCFacade.java
@@ -5,10 +5,39 @@
  */
 package business.NPC;
 
+import business.common.INPCFacade;
+import common.IInventory;
+import common.INPC;
+import common.IPlayer;
+import common.IRoom;
+import java.util.ArrayList;
+
 /**
  *
  * @author andreasmolgaard-andersen
  */
-public class NPCFacade {
+public class NPCFacade implements INPCFacade {
+
+    private ArrayList<NPC> npcs = new ArrayList<>();
+    
+    @Override
+    public IPlayer getPlayer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IRoom[] getRooms() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IInventory[] getInventories() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public INPC[] getNPCs() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
