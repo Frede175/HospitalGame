24a562107390c379d5be820905772e82389ab22a
AndreasBoegh <32385164+AndreasBoegh@users.noreply.github.com>
2017-11-24 13:27:35 +0100

    IItemFacade interface methods added

--- HospitalGame_Business/src/business/common/IItemFacade.java
+++ HospitalGame_Business/src/business/common/IItemFacade.java
@@ -5,10 +5,22 @@
  */
 package business.common;
 
+import common.IInventory;
+import common.IItem;
+import common.IPowerUpItem;
+import java.util.Objects;
+
 /**
  *
  * @author andreasmolgaard-andersen
  */
 public interface IItemFacade {
-    
+    public int createInventory(int maxWeight);
+    public boolean addItem(int inventoryID, IItem item);
+    public boolean removeItem(int inventoryID, IItem item);
+    public boolean activateItem(IItem item);
+    public void update(IPowerUpItem powerUpItem);
+    public IInventory getInventory(int InventoryID);
+    public void load(Objects[] objects);    
 }
+
