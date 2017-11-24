2b466ca8acf55251e5bc026ef74fd99b4776e41a
Jazzmano <robertfrancisti@hotmail.com>
2017-11-24 13:53:10 +0100

    datainventory variables and method headder

--- HospitalGame_Business/src/business/Data/DataInventory.java
+++ HospitalGame_Business/src/business/Data/DataInventory.java
@@ -5,10 +5,32 @@
  */
 package business.Data;
 
+import common.IInventory;
+import common.IItem;
+import common.ItemName;
+import java.util.ArrayList;
+
 /**
  *
  * @author andreasmolgaard-andersen
  */
-public class DataInventory {
+public class DataInventory implements IInventory {
+    private ArrayList<DataItem> items;
+    private Integer maxWeight;
+
+    @Override
+    public IItem getItem(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<IItem> getItems() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IItem[] getItemsByName(ItemName name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
