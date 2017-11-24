a8e41d8dc0e11fdf6b5de796fca8c1b5f9de99af
Jazzmano <robertfrancisti@hotmail.com>
2017-11-24 13:52:09 +0100

    variable and method headder

--- HospitalGame_Business/src/business/Data/DataRoom.java
+++ HospitalGame_Business/src/business/Data/DataRoom.java
@@ -5,10 +5,43 @@
  */
 package business.Data;
 
+import common.Directions;
+import common.IRoom;
+import java.util.HashMap;
+import java.util.Set;
+
 /**
  *
  * @author andreasmolgaard-andersen
  */
-public class DataRoom {
+public class DataRoom implements IRoom{
+    private String name;
+    private HashMap<String, IRoom> exits;
+    private boolean inspected;
+
+    @Override
+    public IRoom getExit(Directions dir) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Set<String> getExitStrings() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isLocked() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isInspected() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
