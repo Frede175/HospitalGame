c8815f4339f024bc6efa1593f638065bf6821471
Frede175 <fsr1998@hotmail.com>
2017-11-24 13:17:17 +0100

    Fixed javafoc

--- HospitalGame_Persistence/src/persistence/PersistenceFacade.java
+++ HospitalGame_Persistence/src/persistence/PersistenceFacade.java
@@ -26,11 +26,27 @@ public class PersistenceFacade implements IPersistence {
         parser = new JsonParser();
     }
 
+    
+    /**
+     * Save the given object to the persistence store.
+     *
+     * @param object the given object to be stored
+     * @return true if the object has been saved and false if object failed to
+     * save.
+     */
     @Override
     public boolean save(Object object) {
         return parser.save(object);
     }
 
+    
+    /**
+     * Get the given object form the persistence store.
+     *
+     * @param <T>
+     * @param type The class that needs to be loaded
+     * @return the an object with the given class or null if an error occurs.
+     */
     @Override
     public <T> T load(Class<T> type) {
         return parser.load(type);
