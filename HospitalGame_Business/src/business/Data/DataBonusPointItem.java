434b18a7f42a333614b1a1ca8ef1dedaba5f8e62
Jazzmano <robertfrancisti@hotmail.com>
2017-11-24 14:08:43 +0100

    added implement "IItem"

--- HospitalGame_Business/src/business/Data/DataBonusPointItem.java
+++ HospitalGame_Business/src/business/Data/DataBonusPointItem.java
@@ -6,17 +6,29 @@
 package business.Data;
 
 import common.IBonusPointItem;
+import common.IItem;
+import common.ItemName;
 
 /**
  *
  * @author andreasmolgaard-andersen
  */
-public class DataBonusPointItem implements IBonusPointItem{
+public class DataBonusPointItem implements IBonusPointItem, IItem{
 
     public int bonusPoints;
     
     @Override
     public int getBonusPoints() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getWeight() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ItemName getName() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
