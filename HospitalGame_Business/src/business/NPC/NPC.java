c82130d216a8217bc2b1c6aaf490035900ca95c5
AndreasBoegh <32385164+AndreasBoegh@users.noreply.github.com>
2017-11-25 13:57:51 +0100

    javadoc, removed some methods andded some

--- HospitalGame_Business/src/business/NPC/NPC.java
+++ HospitalGame_Business/src/business/NPC/NPC.java
@@ -16,43 +16,101 @@ import common.NPCID;
  */
 public class NPC implements INPC {
 
+    /**
+     * the current room of the NPC
+     */
+    private IRoom currentRoom;
+    
+    /**
+     * is the name of the NPC
+     */
     private String name;
+    
+    /**
+     * is the description of the NPC
+     */
     private String description;
-    private boolean CanMove;
+    
+    /**
+     * if the NPC can move is true, false if not
+     */
+    private boolean canMove;
+    
+    /**
+     * is the ID of the NPC
+     */
+    private NPCID npcId;
 
-    public NPC(String name, boolean canMove, IRoom currentRoom) {
-
+    /**
+     * Constructor for NPC
+     * @param name name of the NPC
+     * @param canMove boolean true if the NPC can move
+     * @param currentRoom the room the NPC being created to be in
+     * @param npcId the NPCID of the NPC
+     */
+    public NPC(String name, boolean canMove, IRoom currentRoom, NPCID npcId) {
+        this.name = name;
+        this.canMove = canMove;
+        this.currentRoom = currentRoom;
+        this.npcId = npcId;
     }
 
+    /**
+     * Construcfor for NPC
+     * @param npc is the INPC npc object being constructed to be an NPC instead
+     */
     public NPC(INPC npc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
 
+    /**
+     * sets the room of an NPC
+     * @param currentRoom the new room
+     */
+    public void setCurrentRoom(IRoom currentRoom) {
+        this.currentRoom = currentRoom;
     }
 
-    public void serCurrentRoom(IRoom currentRoom) {
-
-    }
-
+    /**
+     * 
+     * @return description
+     */
     public String getDescription() {
-        return null;
+        return description;
     }
 
     public String interact(IPlayer player) {
-        return null;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * 
+     * @return the currentRoom of NPC
+     */
     @Override
     public IRoom getCurrentRoom() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return currentRoom;
     }
 
+    /**
+     * 
+     * @return the name of NPC
+     */
     @Override
     public String getName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return name;
     }
 
+    /**
+     * 
+     * @return the NPCID of the NPC
+     */
     @Override
     public NPCID getNPCID() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return npcId;
     }
+    
+    
 
 }
