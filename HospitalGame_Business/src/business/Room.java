d71e51235a3f20b51e08850da9a3b24ccd818b71
AndreasBoegh <32385164+AndreasBoegh@users.noreply.github.com>
2017-11-30 14:03:51 +0100

    Room getInventory method implement

--- HospitalGame_Business/src/business/Room.java
+++ HospitalGame_Business/src/business/Room.java
@@ -8,6 +8,7 @@ package business;
 import business.common.IItemFacade;
 import common.Directions;
 import common.ICoordinate;
+import common.IInventory;
 import common.IItem;
 import common.IRoom;
 import java.util.HashMap;
@@ -157,6 +158,11 @@ public class Room implements IRoom {
 
     public void setCoordinate(Coordinate c) {
         this.c = c;
+    }
+
+    @Override
+    public IInventory getInventory() {
+        return itemFacade.getInventory(inventoryID);
     }
 
 }
