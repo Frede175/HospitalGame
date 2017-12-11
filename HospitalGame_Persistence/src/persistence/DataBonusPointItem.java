c2e2dbe0fd2cabb4f174e3cd095cad6a7d8cc69e
Frede175 <fsr1998@hotmail.com>
2017-12-11 22:27:12 +0100

    Javadoc and small fixes

--- HospitalGame_Persistence/src/persistence/DataBonusPointItem.java
+++ HospitalGame_Persistence/src/persistence/DataBonusPointItem.java
@@ -1,29 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package persistence;
 
 import common.IBonusPointItem;
 import java.io.Serializable;
 
 /**
- *
- * @author andreasmolgaard-andersen
+ * Class to store bonus point item
+ * 
+ * @author Frederik Schultz Rosenberg
+ * @author Andreas Bøgh Mølgaard-Andersen
+ * @author Lars Bjerregaard Jørgensen
+ * @author Robert Francisti
  */
 public class DataBonusPointItem extends DataItem implements IBonusPointItem, Serializable {
 
+    /**
+     * How many bonus points the item gives
+     */
     public int bonusPoints;
 
+    /**
+     * Constructor for a bonus point item from the interface IBonusPointItem
+     * @param item the given bonus point item
+     */
     public DataBonusPointItem(IBonusPointItem item) {
         super(item);
         bonusPoints = item.getBonusPoints();
     }
 
-    
-    
-    
+    /**
+     * 
+     * @return the amount of the bonus points.
+     */
     @Override
     public int getBonusPoints() {
         return bonusPoints;
--- HospitalGame_Persistence/src/persistence/PersistenceFacade.java
+++ HospitalGame_Persistence/src/persistence/PersistenceFacade.java
@@ -20,45 +20,89 @@ import java.io.Serializable;
  *
  * @author Frederik Schultz Rosenberg
  * @author Andreas Bøgh Mølgaard-Andersen
- * @author Tobias Ahrenschneider Sztuk
  * @author Lars Bjerregaard Jørgensen
  * @author Robert Francisti
  */
 public class PersistenceFacade implements IPersistence {
 
+    /**
+     * The name all files start with
+     */
     private final String commonName = "data_";
+
+    /**
+     * The file type for all files
+     */
     private final String extension = ".ser";
 
+    /**
+     * Loads a high score from disk.
+     *
+     * @return returns a high score, if the no high score is found the function
+     * returns null.
+     */
     @Override
     public IHighScore getHighScore() {
         return load(DataHighScore.class);
     }
 
+    /**
+     * Saves a game to disk
+     *
+     * @param player The player in the game.
+     * @param inventory All of the inventories in the game.
+     * @param rooms All of the rooms in the game.
+     * @param npcs All of the NPCs in the game.
+     * @return if the game got saved, if an error occurs the function returns
+     * false.
+     */
     @Override
     public boolean saveGame(IPlayer player, IInventory[] inventory, IRoom[] rooms, INPC[] npcs) {
         return save(new DataObject(rooms, player, npcs, inventory));
     }
 
+    /**
+     * Saves a high scores to the disk
+     *
+     * @param highScore the high scores to save
+     * @return if the high scores got saved to the disk, if an error occurs the
+     * function returns false.
+     */
     @Override
     public boolean saveHighScore(IHighScore highScore) {
         return save(new DataHighScore(highScore));
     }
 
+    /**
+     * Loads a game.
+     *
+     * @return a DataObject with all objects to load a game.
+     */
     @Override
     public IDataObject load() {
         return load(DataObject.class);
     }
-    
+
+    /**
+     * Check if there is a saved game on the file system.
+     *
+     * @return true if game i available.
+     */
     @Override
     public boolean saveGameAvailable() {
-        return fileExits(DataObject.class);
+        return fileExists(DataObject.class);
     }
-    
-    
-    private <T> boolean fileExits(Class<T> type) {
+
+    /**
+     * Check if a file exists on the file system from the given type.
+     *
+     * @param <T>
+     * @param type that need to see if a file exits
+     * @return true if the file exits
+     */
+    private <T> boolean fileExists(Class<T> type) {
         return new File(commonName + type.getSimpleName() + extension).exists();
     }
-    
 
     /**
      * Save the given object to the persistence store.
@@ -85,7 +129,7 @@ public class PersistenceFacade implements IPersistence {
      * @return an object with the given class or null if an error occurs.
      */
     private <T extends Serializable> T load(Class<T> type) {
-        if (!fileExits(type)) return null;
+        if (!fileExists(type)) return null;
         T object;
         try (FileInputStream fileIn = new FileInputStream(commonName + type.getSimpleName() + extension)) {
             ObjectInputStream in = new ObjectInputStream(fileIn);
@@ -96,7 +140,4 @@ public class PersistenceFacade implements IPersistence {
         }
         return object;
     }
-
-    
-
 }
